d330cdd3e424cb2fcb47702945d9d6615f0debd4
Anton Djatsuk <adjatsuk@protonmail.com>
2022-08-02 22:28:07 +0100

    Added hover effect

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const body = document.body
 const container = document.querySelector(".container")
 const div = document.querySelector("div")
+const button = document.button
+
+let inputCells = 0; // keeps track of the user input
 let numOfCells = 0; // keeps track of how many cells there are
 
-// Populates the sketch book with a row of user defined length or 16 by default
-const populate = (input) => {
-    input = (typeof input !== 'undefined') ?  input : 16;
+// Gets user input from button, pupulates the grid
+promptMe = () => {
+    let userInput = prompt("Enter the number of cells");
+    let userInputInt = parseInt(userInput);
+    inputCells = userInputInt;
+    populate();
+    addColumns();
+}
+
+// Function for populating the first row of the grid
+const populate = () => {
+    input = (typeof inputCells !== 'undefined' || 0) ?  inputCells : 16;
     for(let i = input; i > 0; i--) {
-        const div = document.createElement("div");
-        container.append(div);
-        div.classList.add('cell')
-        numOfCells += 1;
-        }
+    const div = document.createElement("div");
+    container.append(div);
+    div.classList.add('cell')
+    numOfCells += 1;
+    }
 }
 
 
-// Creates new columns as a multiple of cells in the first grid
+// Function for multiplying the columns by the number of cells in the first grid
 const addColumns = () => {
     for(i = numOfCells; i > 0 + 1; i--) {
     const clonedRows = document.querySelector(".container")
@@ -23,3 +35,8 @@ const addColumns = () => {
     body.append(clone);
     }
 }
+
+// Changes cell color when clicked
+const cell = document.querySelector(".cell")
+cell.addEventListener("click", clickedCell);
+let clickedCell = () => cell.classList.add('.clicked');
