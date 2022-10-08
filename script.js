b72874b97ae1ecbe9e96b259531bc453e386f1e6
Anton Djatsuk <adjatsuk@protonmail.com>
2022-10-08 22:12:43 +0100

    Minor Changes

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const slider = document.getElementById("myRange");
 const inputText = document.getElementById("inputText");
+const wrapper = document.getElementById("wrapper");
+const body = document.body;
 
-let currentValue = () => {
-    inputText.textContent = `Input is ${slider.value}`;
+
+// Insert current range slider value in the the inputText div
+let currentValueText = () => {
+    inputText.textContent = `Number of cells is ${slider.value}`;
+}
+
+let cellContent = () => {
+    for(i = 0; i < Number(slider.value), i++;) {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("grid-element");
+        body.appendChild(cellElement);
+        
     }
+}
 
-slider.addEventListener("input", currentValue);
+// Track the current value in the range slider and output the value as text
+slider.addEventListener("input", currentValueText);
+slider.addEventListener("input", cellContent);
 
