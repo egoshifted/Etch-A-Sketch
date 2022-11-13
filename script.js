70566bab658b48f29671ebb1a2fafd98452dd7c4
Anton Djatsuk <adjatsuk@protonmail.com>
2022-11-13 18:27:39 +0000

    THE RESIZE FUNCTION WORKSgit commit -m!

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-const slider = document.getElementById("myRange");
-const inputText = document.getElementById("inputText");
-const wrapper = document.getElementById("wrapper");
-const body = document.body;
+const defaultSize = 4;
 
+let currentSize = defaultSize
+
+
+const slider = document.getElementById("myRange");
+const sliderInt = parseInt(slider.value);
+const inputText = document.getElementById("inputText");
+const grid = document.getElementById("grid");
+const body = document.body;
+const clearBtn = document.getElementById("clearBtn")
+
+clearBtn.onclick = () => clearGrid()
+
+// Clears grid of all cells
+let clearGrid = () => {
+    grid.innerHTML = ''
+    }
 
 // Insert current range slider value in the the inputText div
 let currentValueText = () => {
-    inputText.textContent = `Number of cells is ${slider.value}`;
-}
-
-let cellContent = () => {
-    for(i = 0; i < Number(slider.value), i++;) {
-        const cellElement = document.createElement("div");
-        cellElement.classList.add("grid-element");
-        body.appendChild(cellElement);
-        
-    }
+    inputText.textContent = `Grid size is ${slider.value} x ${slider.value}`;
+    slider.addEventListener("input", gridSetting(slider.value));
 }
 
 // Track the current value in the range slider and output the value as text
 slider.addEventListener("input", currentValueText);
-slider.addEventListener("input", cellContent);
+
+
+function gridSetting(size) {
+    clearGrid();
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        grid.appendChild(gridElement);
+    }
+       
+}
+
+// Sets grid size to default on window load
+window.onload = () => {
+    gridSetting(defaultSize);
+}
 
